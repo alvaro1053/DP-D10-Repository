e632dfbe5895402573bdf246de061ca7fe9b7ea9
drexpp <ivandega22@gmail.com>
2018-04-13 11:41:54 +0200

    Test de crear y publish newspaper

--- Acme-Newspaper/src/test/java/services/NewspaperServiceTest.java
+++ Acme-Newspaper/src/test/java/services/NewspaperServiceTest.java
@@ -2,6 +2,7 @@ package services;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Date;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -10,9 +11,11 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.Assert;
+import org.springframework.validation.BindingResult;
 
 
 import domain.Newspaper;
+import forms.NewspaperForm;
 
 import utilities.AbstractTest;
 
@@ -106,5 +109,116 @@ public class NewspaperServiceTest extends AbstractTest {
 		checkExceptions(expected, caught);
 		
 	}
+	
+	@Test
+	public void driverCreateNewspaper(){
+		final Date moment = new Date(System.currentTimeMillis());
+		Object testingData[][] = {
+								//Requisitos C: 6.1: Create a newspaper. A user who has created a news paper
+								//is commonly referred to as a publisher
+								//==========================================================================//
+
+								//Tests POSITIVOS 
+								//Poder crear y guardar un newspaper como se haría en el sistema
+								{"user1", "prueba1Title", "Prueba1Description", "https://image.freepik.com/free-photo/cute-cat-picture_1122-449.jpg",moment, false, null},
+								//Tests NEGATIVOS
+								//Intentar crear y guardar un newspaper estando NO registrado
+								{"", "prueba2Title", "Prueba2Description", "https://image.freepik.com/free-photo/cute-cat-picture_1122-449.jpg",moment, false, IllegalArgumentException.class},
+								//Tests NEGATIVOS
+								//Intentar crear y guardar un newspaper con el título en blanco
+								{"user1", "", "Prueba3Description", "https://image.freepik.com/free-photo/cute-cat-picture_1122-449.jpg",moment, false, NullPointerException.class},
+								//Tests NEGATIVOS
+								//Intentar crear y guardar un newspaper con la descripción en blanco
+								{"user2", "prueba4Title", "", "https://image.freepik.com/free-photo/cute-cat-picture_1122-449.jpg",moment, false, NullPointerException.class}
+								
+								
+		};
+		for (int i = 0; i < testingData.length; i++){
+			this.startTransaction();
+			templateCreateNewspaper(((String) testingData[i][0]), (String) testingData[i][1], ((String) testingData[i][2]),((String) testingData[i][3]),((Date) testingData[i][4]), ((Boolean) testingData[i][5]),(Class<?>) testingData[i][6]);
+			this.rollbackTransaction();
+		}
+	}
+
+	protected void templateCreateNewspaper(String username, String title,
+			String description, String pictureURL, Date publicationDate, Boolean privateCheck,
+			Class<?> expected) {
+			Class<?> caught;
+			caught = null;
+			NewspaperForm newspaperForm;
+			Newspaper	newspaper = new Newspaper();
+			
+			try{
+				super.authenticate(username);
+				
+				newspaperForm = new NewspaperForm();
+				
+				newspaperForm.setTitle(title);
+				newspaperForm.setDescription(description);
+				newspaperForm.setPictureURL(pictureURL);
+				newspaperForm.setPublicationDate(publicationDate);
+				newspaperForm.setIsPrivate(privateCheck);
+				
+				BindingResult binding = null;
+				try{
+					newspaper = this.newspaperService.reconstruct(newspaperForm, binding);
+				}catch(Throwable oops){
+					caught = oops.getClass();
+				}
+				
+				this.newspaperService.save(newspaper);
+				
+			}catch(Throwable oops){
+				caught = oops.getClass();
+			}
+			
+			checkExceptions(expected, caught);
+		
+	}
+	
+	
+	@Test
+	public void driverPublishNewspaper(){
+		Object testingData[][] = {
+								//Requisitos C: 6.2: Publish a newspaper that he or she's created. Note that
+								//no newspaper can be published until each of the articles which is composed
+								//is saved in final mode.
+								//==========================================================================//
+
+								//Tests POSITIVOS 
+								//Poder publicar un newspaper, el cual es posible publicar
+								{"user1", "newspaper3", null},
+								//Test NEGATIVO
+								//Un usuario intenta hacer publish de un newspaper que él no ha creado
+								{"user2", "newspaper3", IllegalArgumentException.class},
+								//Un usuario intenta hacer publish de un newspaper que ya esta publicado
+								{"user1", "newspaper1", IllegalArgumentException.class},
+								//Un usuario no registrado NO puede publicar un newspaper
+								{"", "newspaper3", IllegalArgumentException.class}			
+								
+		};
+		for (int i = 0; i < testingData.length; i++){
+			this.startTransaction();
+			templatePublishNewspaper(((String) testingData[i][0]), super.getEntityId((String) testingData[i][1]),(Class<?>) testingData[i][2]);
+			this.rollbackTransaction();
+		}
+	}
+
+	private void templatePublishNewspaper(String username, int newspaperId,
+			Class<?> expected) {
+		Class<?> caught;
+		caught = null;
+		
+		try{
+			super.authenticate(username);
+			Newspaper newspaper = this.newspaperService.findOne(newspaperId);
+			this.newspaperService.publish(newspaper);
+		}catch(Throwable oops){
+			caught = oops.getClass();
+		}
+		
+		checkExceptions(expected, caught);
+	}
+
 
 	}
